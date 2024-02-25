d720926f1eb20f88f1a20b09092f8ebf19783a7b
diamant3 <diamant3@proton.me>
2024-02-25 15:29:38 +0800

    corax+ opcodes passed

--- src/main.v
+++ src/main.v
@@ -10,7 +10,7 @@ struct Vchip8 {
 mut:
     reg [16]u8
     mem [4096]u8
-    gfx [2048]u8
+    gfx []u8
     key [16]u8
     dt u8
     st u8
@@ -52,7 +52,7 @@ fn vchip8_start() &Vchip8 {
         mut vchip8 := &Vchip8 {
         reg: [16]u8 {},
         mem: [4096]u8 {},
-        gfx: [2048]u8 {},
+        gfx: []u8 { len: 2048 },
         key: [16]u8 {},
         dt: 0,
         st: 0,
@@ -85,54 +85,61 @@ fn (mut vchip8 Vchip8) vchip8_load(rom_path string) {
 }
 
 fn (mut vchip8 Vchip8) vchip8_cycle() {
-    for vchip8.pc <= vchip8.mem.len {
-        vchip8.opcode = (u16(vchip8.mem[vchip8.pc]) << 8) | vchip8.mem[(vchip8.pc + 1)]
-        println("[*] opcode: ${vchip8.opcode.hex()}")
+    for vchip8.pc < vchip8.mem.len {
+        vchip8.opcode = u16(vchip8.mem[vchip8.pc]) << 8 | vchip8.mem[vchip8.pc + 1]
+        //println("[*] opcode: ${vchip8.opcode.hex()} PC: ${vchip8.pc.hex()}")
+        // os.execute('cls')
+        // for i in 0..15 {
+        //     eprint(" v${i}: ${vchip8.reg[i]}")
+        // }
+        // eprintln("")
 
 
         match vchip8.opcode & 0xf000 {
             0x0000 {
                 match vchip8.opcode & 0x00ff {
                     0xe0 {
-                        for loc in 0..vchip8.gfx.len {
-                            vchip8.gfx[loc] = 0
-                        }
+                        unsafe { vchip8.gfx.reset() }
                         vchip8.pc += 2
                     }
-                    // 0xee {
-                    // 	vchip8.sp -= 1
-                    // 	vchip8.pc = vchip8.stack[vchip8.sp]
-                    // }
-                    else { println("[!] Unimplemented opcode ${vchip8.opcode}") }
+                    0xee {
+                        vchip8.pc = vchip8.stack[vchip8.sp]
+                        vchip8.stack[vchip8.sp] = 0
+                        vchip8.sp -= 1
+                        vchip8.pc += 2
+                    }
+                    else { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
                 }
             }
-            0x1000 { vchip8.pc = vchip8.opcode & 0x0fff }
-            // 0x2000 {
-            // 	vchip8.sp += 1
-            // 	vchip8.stack[vchip8.sp] = vchip8.pc
-            // 	vchip8.pc = vchip8.opcode & 0x0fff
-            // }
-            // 0x3000 {
-            // 	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 	byte_addr := vchip8.opcode & 0x00ff
+            0x1000 {
+                vchip8.pc = vchip8.opcode & 0x0fff 
+            }
+            0x2000 {
+                vchip8.sp += 1
+                vchip8.stack[vchip8.sp] = vchip8.pc
+            	vchip8.pc = vchip8.opcode & 0x0fff
+            }
+            0x3000 {
+            	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
+            	byte_addr := vchip8.opcode & 0x00ff
 
-            // 	if regx == byte_addr { vchip8.pc += 4 }
-            // 	else { vchip8.pc += 2 }
-            // }
-            // 0x4000 {
-            // 	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 	byte_addr := u8(vchip8.opcode & 0x00ff)
+            	if regx == byte_addr { vchip8.pc += 4 }
+            	else { vchip8.pc += 2 }
+            }
+            0x4000 {
+            	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
+            	byte_addr := u8(vchip8.opcode & 0x00ff)
 
-            // 	if regx != byte_addr { vchip8.pc += 4 }
-            // 	else { vchip8.pc += 2 }
-            // }
-            // 0x5000 {
-            // 	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 	regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            	if regx != byte_addr { vchip8.pc += 4 }
+            	else { vchip8.pc += 2 }
+            }
+            0x5000 {
+            	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
+            	regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
 
-            // 	if regx == regy { vchip8.pc += 4 }
-            // 	else { vchip8.pc += 2 }
-            // }
+            	if regx == regy { vchip8.pc += 4 }
+            	else { vchip8.pc += 2 }
+            }
             0x6000 {
                 byte_addr := u8(vchip8.opcode & 0x00ff)
 
@@ -145,94 +152,95 @@ fn (mut vchip8 Vchip8) vchip8_cycle() {
                 vchip8.reg[(vchip8.opcode & 0x0f00) >> 8] += u8(byte_addr)
                 vchip8.pc += 2
             }
-            // 0x8000 {
-            // 	match vchip8.opcode & 0x000f {
-            // 		0x0 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            0x8000 {
+            	match vchip8.opcode & 0x000f {
+            		0x0 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			vchip8.reg[regx] = vchip8.reg[regy]
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0x1 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.reg[x] = vchip8.reg[y]
+            			vchip8.pc += 2
+            		}
+            		0x1 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			vchip8.reg[regx] |= vchip8.reg[regy]
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0x2 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.reg[x] |= vchip8.reg[y]
+            			vchip8.pc += 2
+            		}
+            		0x2 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			vchip8.reg[regx] &= vchip8.reg[regy]
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0x3 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.reg[x] &= vchip8.reg[y]
+            			vchip8.pc += 2
+            		}
+            		0x3 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			vchip8.reg[regx] ^= vchip8.reg[regy]
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0x4 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.reg[x] ^= vchip8.reg[y]
+            			vchip8.pc += 2
+            		}
+            		0x4 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			result := vchip8.reg[regx] + vchip8.reg[regy]
-            // 			vchip8.reg[regx] += vchip8.reg[regy]
-            // 			if result > 0xff { vchip8.reg[0xf] = 1 }
-            // 			else { vchip8.reg[0xf] = 0 }
+            			result := vchip8.reg[x] + vchip8.reg[y]
+                        vchip8.reg[x] = u8(result)
+            			vchip8.reg[0xf] = if result > 0xff { u8(1) } else { u8(0) }
 
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0x5 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.pc += 2
+            		}
+            		0x5 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			mut vf := u8(0)
-            // 			if vchip8.reg[regx] >= vchip8.reg[regy] { vf = 1 }
+            			mut vf := u8(0)
+            			if vchip8.reg[x] >= vchip8.reg[y] { vf = 1 }
 
-            // 			vchip8.reg[regx] -= vchip8.reg[regy]
-            // 			vchip8.reg[0xf] = vf
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0x6 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			mut vf := vchip8.reg[regx] & 0x1
+            			vchip8.reg[x] -= vchip8.reg[y]
+            			vchip8.reg[0xf] = vf
+            			vchip8.pc += 2
+            		}
+            		0x6 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			mut vf := vchip8.reg[x] & 0x1
 
-            // 			vchip8.reg[regx] >>= 0x1
-            // 			vchip8.reg[0xf] = vf
-            // 		}
-            // 		0x7 {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.reg[x] >>= 0x1
+            			vchip8.reg[0xf] = vf
+                        vchip8.pc += 2
+            		}
+            		0x7 {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 			mut vf := u8(0)
-            // 			if vchip8.reg[regy] >= vchip8.reg[regx] { vf = 1 }
+            			mut vf := u8(0)
+            			if vchip8.reg[y] >= vchip8.reg[x] { vf = 1 }
 
-            // 			vchip8.reg[regx] = vchip8.reg[regy] - vchip8.reg[regx]
+            			vchip8.reg[x] = vchip8.reg[y] - vchip8.reg[x]
 
-            // 			vchip8.reg[0xf] = vf
-            // 			vchip8.pc += 2
-            // 		}
-            // 		0xe {
-            // 			regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 			regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
-            // 			mut vf := vchip8.reg[regx] & 0x80
-            // 			vchip8.reg[regx] = vchip8.reg[regy] << 0x7
-            // 			vchip8.reg[0xf] = vf
-            // 		}
-            // 		else { println("[!] Unimplemented opcode: ${vchip8.opcode}") }
-            // 	}
-            // }
-            // 0x9000 {
-            // 	regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-            // 	regy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+            			vchip8.reg[0xf] = vf
+            			vchip8.pc += 2
+            		}
+            		0xe {
+            			x := (vchip8.opcode & 0x0f00) >> 8
+            			y := (vchip8.opcode & 0x00f0) >> 4
+            			mut vf := (vchip8.reg[x] & 0x80) >> 0x7
+            			vchip8.reg[x] = vchip8.reg[y] << 1
+            			vchip8.reg[0xf] = vf
+                        vchip8.pc += 2
+            		}
+            		else { println("[!] Unimplemented opcode: ${vchip8.opcode.hex()}") }
+            	}
+            }
+            0x9000 {
+            	x := (vchip8.opcode & 0x0f00) >> 8
+            	y := (vchip8.opcode & 0x00f0) >> 4
 
-            // 	if vchip8.reg[regx] != vchip8.reg[regy] { vchip8.pc += 4 }
-            // 	else { vchip8.pc += 2 }
-            // }
+            	if vchip8.reg[x] != vchip8.reg[y] { vchip8.pc += 4 }
+            	else { vchip8.pc += 2 }
+            }
             0xa000 {
                 addr := vchip8.opcode & 0x0fff
 
@@ -250,19 +258,20 @@ fn (mut vchip8 Vchip8) vchip8_cycle() {
             // 	vchip8.reg[regx] = u8(random_number & byte_addr)
             // }
             0xd000 {
-                xx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
-                yy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
+                mut xx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
+                mut yy := vchip8.reg[(vchip8.opcode & 0x00f0) >> 4]
                 height := vchip8.opcode & 0x000f
                 vchip8.reg[0xf] = 0
 
                 for yline in 0..height {
+                    if (yy + yline) > 32 { yy = yy % 32 }
                     pixel := vchip8.mem[vchip8.i + yline]
                     for xline in 0..8 {
-                        if (xx + xline) >= 64 { break }
+                        if (xx + xline) > 64 { xx = xx % 64 }
 
                         if (pixel & (0x80 >> xline)) > 0 {
                             target := (xx + xline) + ((yy + yline) * 64)
-                            if vchip8.gfx[target] == 1 {
+                            if vchip8.gfx[target] > 0 {
                                 vchip8.reg[0xf] = 1
                             }
 
@@ -280,21 +289,45 @@ fn (mut vchip8 Vchip8) vchip8_cycle() {
             // 		else { println("[!] Unimplemented opcode ${vchip8.opcode}") }
             // 	}
             // }
-            // 0xf000 {
-            // 	match vchip8.opcode & 0x00ff {
-            // 		0x07 { println("NOP") }
-            // 		0x0a { println("NOP") }
-            // 		0x15 { println("NOP") }
-            // 		0x18 { println("NOP") }
-            // 		0x1e { println("NOP") }
-            // 		0x29 { println("NOP") }
-            // 		0x33 { println("NOP") }
-            // 		0x55 { println("NOP") }
-            // 		0x65 { println("NOP") }
-            // 		else { println("[!] Unimplemented opcode ${vchip8.opcode}") }
-            // 	}
-            // }
-            else { println("[!] Unimplemented opcode ${vchip8.opcode}") }
+            0xf000 {
+            	match vchip8.opcode & 0x00ff {
+            		0x07 { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
+            		0x0a { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
+            		0x15 { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
+            		0x18 { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
+            		0x1e {
+                        vchip8.i += vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
+                        vchip8.pc += 2
+                    }
+            		0x29 { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
+            		0x33 {
+                       regx := vchip8.reg[(vchip8.opcode & 0x0f00) >> 8]
+                       vchip8.mem[vchip8.i] = regx / 100
+                       vchip8.mem[vchip8.i + 1] = (regx / 10) % 10
+                       vchip8.mem[vchip8.i + 2] = (regx % 10)
+
+                       vchip8.pc += 2
+                    }
+            		0x55 {
+                        vx := (vchip8.opcode & 0x0f00) >> 8
+                        for datum in 0..vx + 1 {
+                            vchip8.mem[vchip8.i + datum] = vchip8.reg[datum]
+                        }
+
+                        vchip8.pc += 2
+                    }
+            		0x65 {
+                        vx := (vchip8.opcode & 0x0f00) >> 8
+                        for datum in 0..vx + 1 {
+                            vchip8.reg[datum] = vchip8.mem[vchip8.i + datum]
+                        }
+
+                        vchip8.pc += 2
+                    }
+            		else { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
+            	}
+            }
+            else { println("[!] Unimplemented opcode ${vchip8.opcode.hex()}") }
         }
     }
 }
@@ -305,7 +338,7 @@ fn main() {
     mut parser := flag.new_flag_parser(os.args)
     parser.application('vchip8')
     parser.version('1.0.0')
-    parser.description('CHIP-8 emulator written in V')
+    parser.description('CHIP-8 interpreter written in V')
     parser.skip_executable()
     rom_path := parser.string(
         'rom',
@@ -346,8 +379,8 @@ fn frame(mut state Vchip8State) {
 
     for x in 0..64 {
         for y in 0..32 {
-            index := x + (64 * y)
-            if state.vchip8.gfx[index] == 1 {
+            index := x + (y * 64)
+            if state.vchip8.gfx[index] > 0 {
                 state.gg.draw_rect_filled(x * 10, y * 10, 10, 10, gx.yellow)
             }
         }
--- v.mod
+++ v.mod
@@ -1,6 +1,6 @@
 Module {
 	name: 'vchip8'
-	description: 'CHIP-8 emulator written in V'
+	description: 'CHIP-8 interpreter written in V'
 	version: '1.0.0'
 	license: 'MIT'
 	dependencies: []
